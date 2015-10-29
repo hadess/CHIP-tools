7a40c04b18356fc1a7496b1b034c4a8e503a1aaa
Bastien Nocera <hadess@hadess.net>
2015-10-29 18:21:18 +0100

    Port verify script to Python 3
    Fedora doesn't ship a Python 2 version of the pyserial module.

--- verify.py
+++ verify.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import io
 import sys
@@ -13,40 +13,40 @@ def answer_prompt(sio,prompt_to_wait_for,answer_to_write,send_cr=True):
   prompt_found = False
   data = ''
   #if send_cr:
-    #sio.write(unicode('\n'))
+    #sio.write('\n')
 
   d='something'
   while not len(d)==0:
     d = sio.read(2000);
     data += d
     time.sleep(1)
-#    print '-' * 50
-#    print ' %d bytes read' % (len(data))
-#    print '-' * 50
+#    print ('-' * 50)
+#    print (' %d bytes read' % (len(data)))
+#    print ('-' * 50)
 
-  #print data
+  #print (data)
 
   line=''
   while not prompt_found:
     d = sio.read(100);
     data += d
-#    print '-' * 50
-#    print ' %d bytes read' % (len(data))
-#    print '-' * 50
-#    print data
-#    print '-' * 50
+#    print ('-' * 50)
+#    print (' %d bytes read' % (len(data)))
+#    print ('-' * 50)
+#    print (data)
+#    print ('-' * 50)
     if len(data.split())>0:
       line=data.split()[-1]
-#    print "matching [%s] against [%s]" % (line,prompt_to_wait_for)
+#    print ("matching [%s] against [%s]" % (line,prompt_to_wait_for))
     if(re.match(prompt_to_wait_for,line,re.M)):
-        sio.write(unicode(answer_to_write+'\n'))
-#        print '-' * 50
-#        print ' detected [%s] ' % prompt_to_wait_for
-#        print '-' * 50
+        sio.write(answer_to_write+'\n')
+#        print ('-' * 50)
+#        print (' detected [%s] ' % prompt_to_wait_for)
+#        print ('-' * 50)
         prompt_found = True
     else:
         if send_cr:
-          sio.write(unicode('\n'))
+          sio.write('\n')
     sio.flush()
     #sys.stdin.readline()
 
@@ -57,15 +57,15 @@ def scanfor(sio,regexp_to_scan_for,answer_to_write):
   data = ''
   while not prompt_found:
     data += sio.read(100);
-#    print '-' * 50
-#    print ' %d bytes read' % (len(data))
-#    print '-' * 50
-#    print data
+#    print ('-' * 50)
+#    print (' %d bytes read' % (len(data)))
+#    print ('-' * 50)
+#    print (data)
     if re.search(regexp_to_scan_for,data):
-#        print '-' * 50
-#        print ' detected [%s] ' % regexp_to_scan_for
-#        print '-' * 50
-        sio.write(unicode(answer_to_write+'\n'))
+#        print ('-' * 50)
+#        print (' detected [%s] ' % regexp_to_scan_for)
+#        print ('-' * 50)
+        sio.write(answer_to_write+'\n')
         prompt_found = True
     sio.flush()
   return data
@@ -80,34 +80,34 @@ def main():
   else:
     serial_port='/dev/ttyACM0';
 
-  #print 'reading from %s:' % serial_port
+  #print ('reading from %s:' % serial_port)
 
   ser = serial.Serial(serial_port,115200, timeout=1);
   sio = io.TextIOWrapper(io.BufferedRWPair(ser,ser))
 
   #login
 
-  print "login...",
+  print ("login..."),
   sys.stdout.flush()
   answer_prompt(sio,'.*login:','chip',True)
-  print "OK\npassword...",
+  print ("OK\npassword..."),
   sys.stdout.flush()
   answer_prompt(sio,'.*Password:','chip',False)
-  print "OK\npoweroff...",
+  print ("OK\npoweroff..."),
   sys.stdout.flush()
   answer_prompt(sio,'.*[\$#]','sudo poweroff')
   answer_prompt(sio,'.*:','chip')
   time.sleep(2)
-  print "OK\n",
+  print ("OK\n"),
   #d=scanfor(sio,r'.*### [^#]+ ###.*','poweroff')
 #  if re.search(r'.*### ALL TESTS PASSED ###.*',d):
-#    print "---> TESTS PASSED"
+#    print ("---> TESTS PASSED")
 #    ser.close();
 #    return 0 
     
   ser.close();
   
-#  print "---> TESTS FAILED"
+#  print ("---> TESTS FAILED")
   return 0
 
 
